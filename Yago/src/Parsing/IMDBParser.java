5a03c8a2589aa6810ee0c1cbaaed4793bba7f2d5
roy klinger <klinger.roy@gmail.com>
2013-05-14 17:03:59 +0300

    added jdbc.jar to project

---
+++ Yago/src/Parsing/IMDBParser.java
@@ -0,0 +1,43 @@
+package Parsing;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+
+public class IMDBParser {
+
+	public final String GENRE_FILE = "genres.list";
+	public final String GENRE_LIST = "8: THE GENRES LIST";
+
+	public void parseGenre(HashMap<String,Movie> moviesTable){
+		//get the current directory
+		String currentDir = System.getProperty("user.dir");
+		//seet the file path
+		String filePath = currentDir+"\\"+GENRE_FILE;
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(filePath));					
+			String[] strArr;
+			@SuppressWarnings("unused")
+			String line , movieName, genre;
+			//read each line
+			while((line = br.readLine()) != null){
+				//if we got the the place in the file with the genres list
+				if (line.contains(GENRE_LIST) && br.readLine().contains("======")){
+					while((line = br.readLine()) != null){
+						//split by tab and get the movie name and genre
+						strArr = line.split("\\t");
+						movieName = strArr[0];
+						genre =strArr[strArr.length-1];
+					}
+					break;
+				}
+			}
+			br.close();
+		}
+		catch(Exception ex){
+			
+		}
+	}
+
+}
