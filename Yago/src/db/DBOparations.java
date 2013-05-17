0ccf16073a69694c26ec22ab9d9e40a2538c43f6
omri <omri@omri-PC>
2013-05-17 15:48:05 +0300

    omri

--- Yago/src/db/DBOparations.java
+++ Yago/src/db/DBOparations.java
@@ -2,37 +2,54 @@ package db;
 
 import java.sql.ResultSet;
 
+import com.mysql.jdbc.Connection;
+
 public class DBOparations implements IdbOparations {
 
-//One row oparations
+
+	Connection conn;
+		
+	//One row operations
+		
+	public DBOparations(Connection connParam){
+		conn = connParam;
+	}
 	
-public DBOparations(){
+	@Override
+	public synchronized int update(String tableNamr, String column, String predicates) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
-}
+	@Override
+	public synchronized int delete(String tableNamr, String whereStatment) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public synchronized int insert(String statment) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public synchronized ResultSet select(String select, String from, String where) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
-@Override
-public int update(String tableNamr, String column, String predicates) {
-	// TODO Auto-generated method stub
-	return 0;
-}
+		@Override
+		public synchronized void importData(String path) {
+			// TODO Auto-generated method stub
 
-@Override
-public int delete(String tableNamr, String whereStatment) {
-	// TODO Auto-generated method stub
-	return 0;
-}
+		}
 
-@Override
-public int insert(String statment) {
-	// TODO Auto-generated method stub
-	return 0;
-}
+		
 
-@Override
-public ResultSet select(String select, String from, String where) {
-	// TODO Auto-generated method stub
-	return null;
-}
-
+	
+	
 
 }
--- Yago/src/db/Import.java
+++ Yago/src/db/Import.java
@@ -1,11 +1,17 @@
 package db;
 
-public class Import implements Iimport {
+public class Import extends Thread implements Iimport  {
 
 	@Override
 	public void importData(String path) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
