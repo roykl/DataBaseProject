58c8b895df1f55cf57120fc110d7021d8131ab50
omri <omri@omri-PC>
2013-05-17 14:48:17 +0300

    omri

--- Yago/src/db/IdbOparations.java
+++ Yago/src/db/IdbOparations.java
@@ -6,8 +6,8 @@ public interface IdbOparations {
 
 	//one row oparations
 	
-	public  int update(String statment);
-	public  int delete(String statment);
+	public  int update(String tableNamr, String column ,String predicates );
+	public  int delete(String tableNamr, String whereStatment /*WHERE some_column=some_value;*/);
 	public  int insert(String statment);
-	public  ResultSet select(String statment);
+	public  ResultSet select(String select, String from, String where);
 }
