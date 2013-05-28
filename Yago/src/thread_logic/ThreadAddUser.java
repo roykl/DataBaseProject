37af6a0d0986699f8abc6d17738bc6e6f0982452
omri sl <omri.sl@gmail.com>
2013-05-28 19:58:04 +0300

    omri

--- Yago/src/thread_logic/ThreadAddUser.java
+++ Yago/src/thread_logic/ThreadAddUser.java
@@ -1,5 +1,50 @@
 package thread_logic;
 
-public class ThreadAddUser extends Thread {
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import db.IdbOparations;
+
+public class ThreadAddUser extends Thread {
+	
+	IdbOparations oparations ;
+	String userName;
+	String pass;
+	
+	public ThreadAddUser(IdbOparations inOpp, String inUserName, String inPass){
+		oparations = inOpp;
+		userName = inUserName;
+		pass = inPass;
+	}
+	
+	//if user not already exist (add user name, pass, and user ID to Users table) return true
+	//return false
+	private boolean addUser(){
+		
+		ResultSet result = oparations.select("idUser", "Users", "idUser = " + userName); 
+		
+		try {
+			//if no such a name than add it
+			if(!result.next()){
+			
+			oparations.insert("Users", Integer.toString(userName.hashCode()) ,userName , pass);
+			return true;
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+		return false;
+		
+	}
+	
+	public void run(){
+		
+		this.addUser();
+	}
+	
 }
--- Yago/src/thread_logic/ThreadGrade.java
+++ Yago/src/thread_logic/ThreadGrade.java
@@ -1,5 +1,101 @@
 package thread_logic;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import db.IdbOparations;
+
 public class ThreadGrade extends Thread {
 
+	IdbOparations oparations ;
+	int IDuser;
+	int IDmovie;
+	int grade;
+	
+	
+	public ThreadGrade(IdbOparations inOpp, int inUser, int inMovie, int inGrade){
+		
+		oparations = inOpp;
+		IDuser = inUser;
+		IDmovie = inMovie;
+		grade = inGrade;
+	}
+	
+	//check if user already ranked the movie
+	private boolean checkRank(){
+		
+		ResultSet result = oparations.select("idUser" ,"UsersMovies", "idUser = " + IDuser + " idMovie = " + IDmovie);
+		
+		try {
+			return result.next();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	//assumption: In MoviesGrades table movie is unique 
+	private void updateAvarage(int IDuser,int IDmovie, int userGrade){
+		
+		
+		int newGrade, newCount;
+		int oldGrade = 0;
+		int  oldCount =0;
+		int oldUserGrade = 0;
+		
+		//get old number of rankers
+		ResultSet result1 = oparations.select("numberOfRankers", "MoviesGrades", "idMovie = "+ IDmovie);
+		//get old grade
+		ResultSet result2 = oparations.select("grade", "MoviesGrades", "idmovie = "+ IDmovie);
+		//get old user grade 
+		ResultSet result3 = oparations.select("rank", "UserMovies", "idUser = "+ IDuser + " idMovie = "+ IDmovie);
+		
+		try {
+			oldCount = result1.getInt(1);
+			oldGrade = result2.getInt(1);
+			oldUserGrade = result3.getInt(1);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(!checkRank()){//not ranked yet
+		newCount = oldCount++;
+		newGrade = ((oldCount*oldGrade) + userGrade)/newCount;
+		}
+		
+		else{//if ranked
+			newCount = oldCount;
+			newGrade = ((oldCount*oldGrade) - oldUserGrade +userGrade)  /newCount;
+		}
+		
+		oparations.insert("MoviesGrades", Integer.toString(IDmovie), Integer.toString(newGrade), Integer.toString(newCount) );
+		
+		
+	}
+	
+	
+	private void grade(){
+		//if not ranked yet
+		if(!checkRank()){
+			//update user-movie table
+			oparations.insert("UsersMovies", Integer.toString(IDuser), Integer.toString(IDmovie), Integer.toString(grade));
+		}
+		
+		//update movie-grade
+		updateAvarage(IDuser, IDmovie, grade);		
+		
+		
+		
+	}
+	
+	public void run(){
+		
+		this.grade();
+		
+	}
+	
+	
 }
